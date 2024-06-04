89721a3a120a8bb5afe943bf46c6fe13e8fc818f
0xbrax <marco.braccini@live.it>
2024-06-04 12:19:56 +0200

    favicon

--- src/scripts/game/Balance.js
+++ src/scripts/game/Balance.js
@@ -7,7 +7,7 @@ export class Balance extends SimpleSprite {
     constructor(scaleFactor, bodyContainer) {
         const xGap = 105 * scaleFactor;
 
-        super("BalanceImage", scaleFactor + (0.15 * scaleFactor), bodyContainer, 72, xGap);
+        super("BalanceImage", scaleFactor + (0.15 * scaleFactor), bodyContainer, 71, xGap);
 
         this.sprite.rotation = -0.07;
         this.setText();
